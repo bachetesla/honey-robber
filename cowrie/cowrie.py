785240b5334507d70f3442c90ad616355f06705f
Arian <itsarianebrahimi@gmail.com>
2022-01-03 19:10:54 +0330

    Add cowrie initial files

---
+++ cowrie/cowrie.py
@@ -0,0 +1,11 @@
+"""
+cowrie/cowrie is a simple honeypot
+"""
+
+from honey_robber.honey_robber import HoneyRobber
+
+
+class cowrie(HoneyRobber):
+    """
+    This Class is using cowrie/cowrie
+    """
